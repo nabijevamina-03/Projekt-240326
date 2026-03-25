a20e439d4ebf3ce65f37cf3db91825770f111554
Farida Bamatkhanova <nabijevamina@gmail.com>
2026-03-25 10:04:17 +0100

    mieten und wartung hinzugefügt

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,36 @@ import { EBike } from "./ebike";
 // Manager erstellen
 const manager = new FlottenManager();
 
-// Fahrzeuge hinzufügen
-manager.fahrzeugHinzufuegen(new EScooter(1, "Scooter 1", 10));
-manager.fahrzeugHinzufuegen(new EBike(2, "Bike 1", 50));
+// Fahrzeuge erstellen
+const scooter = new EScooter(1, "Scooter 1", 10);
+const bike = new EBike(2, "Bike 1", 50);
+
+// hinzufügen
+manager.fahrzeugHinzufuegen(scooter);
+manager.fahrzeugHinzufuegen(bike);
 
 // Alle anzeigen
-console.log("Vor dem Laden:");
+console.log("Alle Fahrzeuge:");
 console.log(manager.alleFahrzeugeAnzeigen());
 
-// Leere Fahrzeuge laden (<20%)
+// Mieten testen
+console.log("\nScooter wird gemietet:");
+scooter.rent();
+
+// Wartung testen
+console.log("\nScooter mehrfach mieten:");
+for (let i = 0; i < 5; i++) {
+  scooter.rent();
+}
+
+// Reparieren
+console.log("\nScooter wird repariert:");
+scooter.repair();
+
+// Laden
+console.log("\nVor dem Laden:");
+console.log(manager.alleFahrzeugeAnzeigen());
+
 manager.leereFahrzeugeLaden();
 
 console.log("Nach dem Laden:");
